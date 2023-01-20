87f161696d5cd89ed0aadda4dcfcb1351224cc07
Nitish Kumar <73831063+nitish74@users.noreply.github.com>
2023-01-20 16:20:39 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,17 @@
+document.querySelector("form").addEventListener("submit",accreate)
+
+function accreate(){
+Event.preventDefault();
+
+var userData={
+    Name:document.querySelector("#name").value,
+    Email:document.querySelector("#email").value,
+    Password:document.querySelector("#pass").value,
+    Accesscode:document.querySelector("#access").value,
+}
+
+console.log("fghfghj");
+localStorage.setItem("userData",JSON.stringify(userData));
+alert("Account Created Successfully...Redirecting to Login's Page");
+window.location.href="signin.html";
+}
